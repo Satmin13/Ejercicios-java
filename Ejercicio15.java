cf7fb7a1947de18fadce7b1d4a43f5dfdd0eb7ab
Satmin13 <xiky13@hotmail.com>
2016-10-19 09:09:48 +0200

    Add files via upload

---
+++ Ejercicio15.java
@@ -0,0 +1,6 @@
+/**Ejercicio 15
+Escribe un programa que pinte una pirámide rellena con un carácter introducido
+por teclado que podrá ser una letra, un número o un símbolo como *, +,
+-, $, &, etc. El programa debe permitir al usuario mediante un menú elegir si
+el vértice de la pirámide está apuntando hacia arriba, hacia abajo, hacia la
+izquierda o hacia la derecha.**/
