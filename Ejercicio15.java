948156a7d49af16b1f0689afd5ced160141f8a53
Satmin13 <xiky13@hotmail.com>
2016-10-19 10:06:55 +0200

    Add files via upload

--- Ejercicio15.java
+++ Ejercicio15.java
@@ -4,3 +4,23 @@ por teclado que podrá ser una letra, un número o un símbolo como *, +,
 -, $, &, etc. El programa debe permitir al usuario mediante un menú elegir si
 el vértice de la pirámide está apuntando hacia arriba, hacia abajo, hacia la
 izquierda o hacia la derecha.**/
+public class Ejercicio15{
+	public static void main(String[]args){
+		System.out.println("Escribe una letra o símbolo para que dibujemos una pirámide");
+		String car = System.console().readLine();
+		System.out.println("Escribe a si quieres que la pirámide tenga su vértice arriba y b si lo quieres abajo!");
+		String posicion = System.console().readLine();		
+		if (posicion.equals("a")){
+			System.out.println("     "+ car );
+			System.out.println("    "+ car + car + car );
+			System.out.println("   "+ car + car + car + car + car + car);
+			System.out.println("  "+ car + car + car + car + car + car + car + car);
+		}
+		if (posicion.equals("b")){
+			System.out.println("  "+ car + car + car + car + car + car + car + car);			
+			System.out.println("   "+ car + car + car + car + car + car);
+			System.out.println("    "+ car + car + car );
+			System.out.println("     "+ car );
+		} else {System.out.println("Perdón no lo he entendido :(");}
+	}
+}
